96510e544769f90fc755fb551fef525fb46dab3b
notpike <e1000basetx@gmail.com>
2020-06-11 22:58:15 -0400

    Update

--- utils/TX.py
+++ utils/TX.py
@@ -11,12 +11,12 @@ class TX:
     def __init__(self, gpio=17):
         self.GPIO = gpio
         if(os.path.isdir("/sys/class/gpio/gpio$GPIO") == False):
-            os.system("echo " + gpio + " > /sys/class/gpio/export")
+            os.system("echo " + str(self.GPIO) + " > /sys/class/gpio/export")
             os.system("echo \"out\" >  /sys/class/gpio/gpio$GPIO/direction")
 
 
     def txOn(self):
-        os.system("echo \"1\" > /sys/class/gpio/gpio" + self.GPIO + "/value")
+        os.system("echo \"1\" > /sys/class/gpio/gpio" + str(self.GPIO) + "/value")
 
     def txOff(self):
-        os.system("echo \"0\" > /sys/class/gpio/gpio" + self.GPIO + "/value")
+        os.system("echo \"0\" > /sys/class/gpio/gpio" + str(self.GPIO) + "/value")
