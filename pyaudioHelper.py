1194d76c4fdf2365900166cdec4c8aa847615a63
notpike <e1000basetx@gmail.com>
2020-06-16 23:56:16 -0400

    pyaudio helper

--- pyaudioHelper.py
+++ pyaudioHelper.py
@@ -6,4 +6,4 @@ info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name')
+            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
